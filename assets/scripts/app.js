7c42001f756355e211986cb505bf6abf9cfbca29
eNeville <17412822+eNeville@users.noreply.github.com>
2022-12-19 18:02:09 -0400

    Add closeMenuHandler

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,15 @@
 const menu = document.getElementById('mobile-menu');
 const openHandler = () => {
-  menu.classList.remove('hide');
+  menu.classList.remove('hide-menu');
 };
 const menuIcon = document.getElementById('menu-icon');
 menuIcon.addEventListener('click', openHandler);
+
+const closeMenu = document.querySelector('.close-menu');
+
+const closeMenuHandler = () => {
+  console.log('Hello');
+  menu.classList.add('hide-menu');
+};
+
+closeMenu.addEventListener('click', closeMenuHandler);
