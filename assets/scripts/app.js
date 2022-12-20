e4706b90ada53562f026944dee3333b311759430
Hans-Derly Rameau <rameauhansderly@gmail.com>
2022-12-20 12:32:09 -0400

    prevent scrolling on mobile menu

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,13 +4,19 @@ const closeMenu = document.querySelector('.close-menu');
 const mobileLinks = document.getElementById('mobile-links');
 
 const openHandler = () => {
-  menu.classList.remove('hide-menu');
+	menu.classList.remove('hide-menu');
+	document.body.classList.add('stop-scrolling');
+	document.body.addEventListener('touchmove', (e) => {
+		e.preventDefault();
+	});
 };
 
 menuIcon.addEventListener('click', openHandler);
 
 const closeMenuHandler = () => {
-  menu.classList.add('hide-menu');
+	menu.classList.add('hide-menu');
+	document.body.removeEventListener('touchmove');
+	document.body.classList.remove('stop-scrolling');
 };
 
 closeMenu.addEventListener('click', closeMenuHandler);
