2857d93da2703bb738948b7f49af7afd9a3b7fed
Hans-Derly Rameau <rameauhansderly@gmail.com>
2022-12-20 17:21:12 -0400

    add card dynamicly

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,7 +17,7 @@ closeMenu.addEventListener('click', closeMenuHandler);
 
 mobileLinks.addEventListener('click', closeMenuHandler);
 
-const workCardDetail = [
+const recentWorks = [
   {
     id: 0,
     title: 'Multi-Post Stories Gain+Glory',
@@ -49,3 +49,37 @@ const workCardDetail = [
     tags: ['Ruby', 'Css', 'JavaScript', 'Html'],
   },
 ];
+
+
+const addWorkCard = () => {
+  const recentWorkGridContainer = document.querySelector(
+    '.recent-work-grid-container',
+  );
+  recentWorks.map((work) => {
+    const card = document.createElement('div');
+    card.classList.add('single-card-container');
+    card.innerHTML = `
+                <div class="single-card">
+                    <div class="img-single-work"></div>
+                    <div class="infos-container">
+                        <header class="name-single-work">
+                            <h3>${work.title}</h3>
+                        </header>
+                        <div class="stack-single-work-container">
+                            <ul class="stack-single-container">
+                                ${work.tags.map((w) => `<li class="stack"><a href="#">${w}</a></li>`)}
+                            </ul>
+                        </div>
+                        <div class="see-project-container">
+                            <button class="see-project-text see-project btn" id="${work.id}" type="button">
+                                See Project
+                            </button>
+                        </div>
+                    </div>
+
+    `;
+    recentWorkGridContainer.appendChild(card);
+  });
+};
+
+addWorkCard();
