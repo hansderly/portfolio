7980abbb4535df3947b6a060c7fce4396d2a8719
Hans-Derly Rameau <rameauhansderly@gmail.com>
2022-12-21 16:30:20 -0400

    add email validation logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -275,4 +275,3 @@ const addEventListenerOnSeeProjectButton = () => {
 };
 
 addEventListenerOnSeeProjectButton();
-
--- assets/scripts/validate.js
+++ assets/scripts/validate.js
@@ -1,2 +1,16 @@
-const form = document.getElementById('contact-form');
+const form = document.getElementById('form');
 const { email } = form.elements;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (email.value !== email.value.toLowerCase()) {
+    const alertContainer = document.querySelector('.alert-container');
+    const alertMessage = document.querySelector('#alertMessage');
+    alertContainer.classList.remove('hide');
+    alertMessage.textContent = 'Email is not valid, must be lowercase';
+    return;
+  }
+
+  form.submit();
+});
