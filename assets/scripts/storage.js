36e12423bf607a805c17b90e1bb3058180fe3894
Hans-Derly Rameau <rameauhansderly@gmail.com>
2022-12-22 23:08:59 -0400

    fix eslint error for storage.js file

--- assets/scripts/storage.js
+++ assets/scripts/storage.js
@@ -1,14 +1,15 @@
+/* eslint-disable camelcase */
 const form = document.querySelector('#form');
 const { fullName: full_name, email, message } = form.elements;
 
 const setFormDataToLocalStorage = () => {
-	const full_name_input = full_name.value;
-	const email_input = email.value;
-	const message_input = message.value;
+  const full_name_input = full_name.value;
+  const email_input = email.value;
+  const message_input = message.value;
 
-	const data = { full_name_input, email_input, message_input };
-	const data_stringify = JSON.stringify(data);
-	localStorage.setItem('form_input_data', data_stringify);
+  const data = { full_name_input, email_input, message_input };
+  const data_stringify = JSON.stringify(data);
+  localStorage.setItem('form_input_data', data_stringify);
 };
 
 full_name.addEventListener('change', () => setFormDataToLocalStorage());
@@ -16,17 +17,17 @@ email.addEventListener('change', () => setFormDataToLocalStorage());
 message.addEventListener('change', () => setFormDataToLocalStorage());
 
 const populateDataToForm = () => {
-	const data = localStorage.getItem('form_input_data');
-	const data_parse = JSON.parse(data);
+  const data = localStorage.getItem('form_input_data');
+  const data_parse = JSON.parse(data);
 
-	if (data?.length) {
-		const { full_name_input, email_input, message_input } = data_parse;
-		full_name.value = full_name_input || '';
-		email.value = email_input || '';
-		message.value = message_input || '';
-	}
+  if (data?.length) {
+    const { full_name_input, email_input, message_input } = data_parse;
+    full_name.value = full_name_input || '';
+    email.value = email_input || '';
+    message.value = message_input || '';
+  }
 };
 
 window.onload = () => {
-	populateDataToForm();
+  populateDataToForm();
 };
