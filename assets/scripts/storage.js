28988ef907cd774f1a896b49525f3cf4b161e78a
Hans-Derly Rameau <rameauhansderly@gmail.com>
2022-12-22 22:49:57 -0400

    set form data object to local storage when input change

--- assets/scripts/storage.js
+++ assets/scripts/storage.js
@@ -1,2 +1,16 @@
 const form = document.querySelector('#form');
 const { fullName: full_name, email, message } = form.elements;
+
+const setFormDataToLocalStorage = () => {
+	const full_name_input = full_name.value;
+	const email_input = email.value;
+	const message_input = message.value;
+
+	const data = { full_name_input, email_input, message_input };
+	const data_stringify = JSON.stringify(data);
+	localStorage.setItem('form_input_data', data_stringify);
+};
+
+full_name.addEventListener('change', () => setFormDataToLocalStorage());
+email.addEventListener('change', () => setFormDataToLocalStorage());
+message.addEventListener('change', () => setFormDataToLocalStorage());
